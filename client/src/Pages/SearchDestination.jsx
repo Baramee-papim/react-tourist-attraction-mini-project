629f2100e95f7825ffd694944dbe8d72115c29bb
baramee <baramee.papim@gmail.com>
2026-01-16 17:25:10 +0700

    refactor: simplify search handling in SearchDestinationPage

--- client/src/Pages/SearchDestination.jsx
+++ client/src/Pages/SearchDestination.jsx
@@ -1,21 +1,14 @@
 import CardSection from "../component/CardSection";
 import SearchInput from "../component/SearchInput";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function SearchDestinationPage() {
   const [search, setSearch] = useState("");
 
-  // debounce เล็กน้อย ลดการยิง request ถี่เกินไป
-  const [debouncedSearch, setDebouncedSearch] = useState(search);
-  useEffect(() => {
-    const t = setTimeout(() => setDebouncedSearch(search), 250);
-    return () => clearTimeout(t);
-  }, [search]);
-
   return (
     <div className="App min-h-screen bg-gray-100 py-8">
       <SearchInput search={search} onSearchChange={setSearch} />
-      <CardSection keywords={debouncedSearch} />
+      <CardSection keywords={search} />
     </div>
   );
 }
